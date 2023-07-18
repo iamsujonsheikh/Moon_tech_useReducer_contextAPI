6d7e82c8fd482048c8940c16e5e50a1111284e93
sujonphero <sujonsheikh.in@gmail.com>
2023-07-18 11:48:08 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
+import ProductProvider from "./context/ProductProvider";
 
 function App() {
+
+
   return (
     <div>
-      <RouterProvider router={routes} />
+      <ProductProvider>
+        <RouterProvider router={routes} />
+      </ProductProvider>
     </div>
   );
 }
